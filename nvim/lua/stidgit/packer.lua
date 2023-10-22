b9e76ea8d8ea50d33a84cf1365c4c833ea12916c
Stidgit <alfred.xueke@gmail.com>
2023-10-23 01:10:06 +0300

    LSP

--- nvim/lua/stidgit/packer.lua
+++ nvim/lua/stidgit/packer.lua
@@ -24,4 +24,19 @@ return require('packer').startup(function(use)
 
   use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use ('mbbill/undotree')
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v3.x',
+	  requires = {
+		  -- LSP Support
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+		  {'neovim/nvim-lspconfig'},
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'L3MON4D3/LuaSnip'},
+	  }
+  }
 end)
