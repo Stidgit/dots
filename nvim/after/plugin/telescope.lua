953d0465685617e485582325b4d597c137c5deae
Stidgit <alfred.xueke@gmail.com>
2023-10-25 00:20:39 +0300

    nvim tweaks

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,5 +4,3 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep : ") })
 end)
-
-vim.o.number = true
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,4 @@
 print("Welcome back to Neovim Stid!")
 require("stidgit")
+vim.o.number = true
+vim.api.nvim_create_autocmd("BufEnter", { callback = function() vim.opt.formatoptions = vim.opt.formatoptions - { "c","r","o" } end, })
