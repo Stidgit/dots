906ee44f3eebf7a44eb7e1cae707a1085e7a5efd
Stidgit <alfred.xueke@gmail.com>
2023-10-31 00:41:49 +0200

    config changes

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,6 +8,6 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar
 # Bar is the name set in the polybar config, so if you change it, you have to change it here too.
-polybar bar
+polybar
 
 echo "Bars launched..."
